2657eea5eb674eaf46f20d0bbf082ab628bd799e
Pedro Estape <pedro.estape@gmail.com>
2026-02-10 13:42:47 -0300

    added checkout button

--- backend/src/controllers/RestaurantController.ts
+++ backend/src/controllers/RestaurantController.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
+
+const getRestaurant = async (req: Request, res: Response) => {
+    try {
+        const restaurantId = req.params.restaurantId;
+
+        const restaurant = await Restaurant.findById(restaurantId);
+
+        if (!restaurant) {
+            return res.status(404).json({message: "Restaurant not found"});
+        }
+
+        res.json(restaurant);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Something went wrong"});
+    }
+};
+
 const searchRestaurants = async (req: Request, res: Response) => {
     try {
         const city = req.params.city;
@@ -62,5 +81,6 @@ const searchRestaurants = async (req: Request, res: Response) => {
 };
 
 export default {
+    getRestaurant,
     searchRestaurants
 };
--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -4,7 +4,7 @@ import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
-// router.get("/:restaurantId", param("restaurantId").isString().trim().notEmpty().withMessage("Restaurant ID parameter must be a valid string"),)
+router.get("/:restaurantId", param("restaurantId").isString().trim().notEmpty().withMessage("Restaurant ID parameter must be a valid string"), RestaurantController.getRestaurant);
 router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"), RestaurantController.searchRestaurants);
-//test
+
 export default router;
