e0b15ddadbbbe249a60e543b35220b82a0c8a78d
Pedro Estape <pedro.estape@gmail.com>
2026-02-25 13:04:39 -0300

    added stripe checkout session flow

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Stripe from "stripe";
 import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -20,23 +21,45 @@ type CheckoutSessionRequest = {
     restaurantId: string;
 };
 
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+    console.log("RECEIVED EVENT");
+    console.log("==============");
+    console.log("event: ", req.body);
+    
+    res.send();
+};
+
 const createCheckoutSession = async (req: Request, res: Response) => {
     try {
         const checkoutSessionRequest: CheckoutSessionRequest = req.body;
 
+        console.log(checkoutSessionRequest.cartItems);
+        
         const restaurant = await Restaurant.findById(checkoutSessionRequest.restaurantId);
 
         if (!restaurant) {
             throw new Error("Restaurant not found");
         }
+        
+
+        const newOrder = new Order({
+            restaurant: restaurant,
+            user: req.userId,
+            status: "placed",
+            deliveryDetails: checkoutSessionRequest.deliveryDetails,
+            cartItems: checkoutSessionRequest.cartItems,
+            createdAt: new Date()
+        });
 
         const lineItems = createLineItems(checkoutSessionRequest, restaurant.menuItems);
 
-        const session = await createSession(lineItems, "TEST_ORDER_ID", restaurant.deliveryPrice, restaurant._id.toString());
+        const session = await createSession(lineItems, newOrder._id.toString(), restaurant.deliveryPrice, restaurant._id.toString());
 
         if (!session.url) {
             return res.status(500).json({message: "Error creating stripe session"})
         }
+
+        await newOrder.save();
 
         res.json({ url: session.url });
         
@@ -101,5 +124,6 @@ const createSession = async (lineItems: Stripe.Checkout.SessionCreateParams.Line
 
 
 export default {
-    createCheckoutSession
+    createCheckoutSession,
+    stripeWebhookHandler
 };
--- backend/src/routes/OrderRoute.ts
+++ backend/src/routes/OrderRoute.ts
@@ -6,5 +6,6 @@ import OrderController from "../controllers/OrderController";
 const router = express.Router();
 
 router.post("/checkout/create-checkout-session", jwtCheck, jwtParse , OrderController.createCheckoutSession);
+router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
 
 export default router;
